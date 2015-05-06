5d5ca13463b41558eaf6ff663a2112ad526d1de9
William Decker <wdw1062@gaintjoe.edu>
2015-05-06 11:51:33 -0500

    doesnt work cuz of stupid bug

--- date.js
+++ date.js
@@ -1,8 +1,16 @@
 function findDate(string) {
   var dateTime = /(\d{1,2})-(\d{1,2})-(\d{4})/;
   var match = dateTime.exec(string);
-  return new Date(Number(match[3]),
+  if (string = ("30-01-2003")) {
+    return new Date(Number(match[3]),
                   Number(match[2]) - 1,
                   Number(match[1]));
-}
-print(findDate("30-1-2003"));
+  }
+  else if (string = ("01-30-2003")) {
+	return new Date(Number(match[2]) - 1,
+                    Number(match[3]),
+                     Number(match[1]));
+  }
+};
+print(findDate("01-30-2003"));
+print(findDate("30-01-2003"));
